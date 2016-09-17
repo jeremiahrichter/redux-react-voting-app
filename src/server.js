53da90627d1b55e0d79bee13e3910e621620f0b0
jeremiah <jeremiahrichter@gmail.com>
2016-09-17 17:35:14 -0500

    add subscription to store

--- src/server.js
+++ src/server.js
@@ -3,4 +3,8 @@ import Server from 'socket.io';
 export function startServer(store) {
     const io = new Server().attach(8090);
     console.log('Server started on port 8090.');
+
+    store.subscribe(() => {
+        io.emit('state', store.getState().toJS());
+    });
 }
