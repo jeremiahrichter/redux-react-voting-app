4f77bb0b6594178d1aea06e4d9e166ac41e4fe75
jeremiah <jeremiahrichter@gmail.com>
2016-09-17 14:32:15 -0500

    add next function

--- src/core.js
+++ src/core.js
@@ -1,5 +1,17 @@
-import {List} from 'immutable';
+import {
+    List
+} from 'immutable';
 
 export function setEntries(state, entries) {
-  return state.set('entries', List(entries));
+    return state.set('entries', List(entries));
+}
+
+export function next(state) {
+    var entries = state.get('entries');
+    return state.merge({
+        vote: Map({
+            pair: entries.take(2)
+        }),
+        entries: entries.skip(2)
+    });
 }
