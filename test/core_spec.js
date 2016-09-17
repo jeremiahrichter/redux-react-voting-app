ec209d5369cb03352c27d959c1db38e4c1851d71
jeremiah <jeremiahrichter@gmail.com>
2016-09-17 16:08:19 -0500

    mare reducer pass tests

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,7 @@
 import {
     List,
-    Map
+    Map,
+    fromJS
 } from 'immutable';
 import {
     expect
@@ -9,6 +10,8 @@ import {
 import {
     setEntries, next, vote
 } from '../src/core';
+
+import reducer from '../src/reducer';
 
 describe('application logic', () => {
 
@@ -142,5 +145,53 @@ describe('application logic', () => {
                 entries: List()
             }));
         });
+    });
+
+    describe('reducer', () => {
+
+        it('handles SET_ENTRIES', () => {
+            const initialState = Map();
+            const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+            const nextState = reducer(initialState, action);
+
+            expect(nextState).to.equal(fromJS({
+                entries: ['Trainspotting']
+            }));
+        });
+
+        it('handles NEXT', () => {
+            const initialState = fromJS({
+                entries: ['Trainspotting', '28 Days Later']
+            });
+            const action = {type: 'NEXT'};
+            const nextState = reducer(initialState, action);
+
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: ['Trainspotting', '28 Days Later']
+                },
+                entries: []
+            }));
+        });
+
+        it('handles VOTE', () => {
+            const initialState = fromJS({
+                vote: {
+                    pair: ['Trainspotting', '28 Days Later']
+                },
+                entries: []
+            });
+            const action = {type: 'VOTE', entry: 'Trainspotting'};
+            const nextState = reducer(initialState, action);
+
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: ['Trainspotting', '28 Days Later'],
+                    tally: {Trainspotting: 1}
+                },
+                entries: []
+            }));
+        });
+
     });
 });
