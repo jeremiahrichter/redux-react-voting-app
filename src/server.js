90319cb14924263b1a22bb7294a363562e68a4ed
jeremiah <jeremiahrichter@gmail.com>
2016-09-17 17:27:00 -0500

    fix import bug and add some loggin

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import {Server} from 'socket.io';
+import Server from 'socket.io';
 
 export default function startServer() {
     const io = new Server().attach(8090);
+    console.log('Server started on port 8090.');
 }
